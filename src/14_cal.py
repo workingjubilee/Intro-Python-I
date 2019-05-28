17769a3ab07c83b19c15dc69f466b223171e5071
workingjubilee <workingjubilee@gmail.com>
2019-05-28 16:19:23 -0700

    Finishes assignment with try/except flow

--- src/14_cal.py
+++ src/14_cal.py
@@ -25,22 +25,49 @@ from datetime import datetime
 
 cal = TextCalendar()
 
-def globalist_month():
-  global query_month
-  try:
-    query_month = int(sys.argv[1])
-  except:
-    query_month = datetime.today().month
+UsageError = Exception("""
+  Please provide a month and then year, as numbers, e.g. python3 14_cal.py 5 2019
+  Or provide nothing at all to get the current month's calendar.
+  """)
 
-def globalist_year():
-  global query_year
-  try:
-    query_year = int(sys.argv[2])
-  except:
-    query_year = datetime.today().year
-    
+try:
+    if len(sys.argv) == 1:
+        query_month = datetime.today().month
+        query_year = datetime.today().year
+    elif len(sys.argv) == 2:
+        query_month = int(sys.argv[1])
+        query_year = datetime.today().year
+    elif len(sys.argv) == 3:
+        query_month = int(sys.argv[1])
+        query_year = int(sys.argv[2])
+    else:
+        raise UsageError
 
-globalist_month()
-globalist_year()
+    cal.prmonth(query_year, query_month)
 
-print(cal.prmonth(query_year, query_month))
+except:
+    print(UsageError)
+
+
+# let proper_usage = Exception('Text ')
+
+# def globalist_month():
+#   global query_month
+#   try:
+#     query_month = int(sys.argv[1])
+#   except:
+#   finally:
+#     query_month = datetime.today().month
+
+# def globalist_year():
+#   global query_year
+#   try:
+#     query_year = int(sys.argv[2])
+#   except:
+#     query_year = datetime.today().year
+
+
+# globalist_month()
+# globalist_year()
+
+# print(cal.prmonth(query_year, query_month))
